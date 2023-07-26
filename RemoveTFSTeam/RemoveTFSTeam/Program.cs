005beebc36b2796195b3c77040c01a3fb193956e
Rod Falanga <127354181+Rod-at-DOH@users.noreply.github.com>
2023-07-26 14:31:03 -0600

    got rid of a debugging line

--- RemoveTFSTeam/RemoveTFSTeam/Program.cs
+++ RemoveTFSTeam/RemoveTFSTeam/Program.cs
@@ -6,8 +6,6 @@
     // This is a part of the process of migrating TFS/TFVC projects to Git repos.
     private static void Main(string[] args)
     {
-        Console.WriteLine($"args.Length == {args.Length}");
-
         string s;
 
         // Is this app being fed data on the PowerShell command line, or is it being redirected data using a PowerShell pipe operator (|)?
