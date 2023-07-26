d5897598a098d18a550b73f7bdf5d47d72d390e2
Rod Falanga <127354181+Rod-at-DOH@users.noreply.github.com>
2023-07-26 13:03:10 -0600

    I realized that I'm going to need the args parameter
    So I converted this to a Program.Main app

--- RemoveTFSTeam/RemoveTFSTeam/Program.cs
+++ RemoveTFSTeam/RemoveTFSTeam/Program.cs
@@ -1,5 +1,11 @@
-﻿// This application is meant to be used for removing the section of code:
-// "GlobalSection(TeamFoundationVersionControl)" to "EndGlobalSection"
-// from the Visual Studio .sln files, which are passed to it on the PowerShell command line.
-// This is a part of the process of migrating TFS/TFVC projects to Git repos.
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    // This application is meant to be used for removing the section of code:
+    // "GlobalSection(TeamFoundationVersionControl)" to "EndGlobalSection"
+    // from the Visual Studio .sln files, which are passed to it on the PowerShell command line.
+    // This is a part of the process of migrating TFS/TFVC projects to Git repos.
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
